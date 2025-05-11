a771c3e92609955caed5bc858a406ebb960f5c3f
minxrka <minorka04@mail.ru>
2025-05-11 16:57:44 +0300

    *api *constsnts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -4,26 +4,16 @@ import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: `${BASE_URL}/api`,
-  credentials: "include", //куки
+  credentials: "include",
+  mode: "cors",
   prepareHeaders: (headers, { getState }) => {
     const token =
       (getState() as RootState).user.token || localStorage.getItem("token")
-
     if (token) {
       headers.set("Authorization", `Bearer ${token}`)
     }
-
-    // CORS
-    headers.set("Access-Control-Allow-Origin", "*")
-    headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    )
-    headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
     return headers
   },
-  mode: "cors",
 })
 
 const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 })
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,1 @@
-// constants.ts
-export const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  (import.meta.env.MODE === "production"
-    ? "https://express-api-social-network.onrender.com"
-    : "http://localhost:3000")
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
